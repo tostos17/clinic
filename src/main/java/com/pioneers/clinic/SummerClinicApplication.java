bf531a9989fd1d8afe61633e1200760f7a651c08
oluwatosin <fowobiy2k@yahoo.com>
2025-07-29 05:15:02 +0100

    removed player list

--- src/main/java/com/pioneers/clinic/SummerClinicApplication.java
+++ src/main/java/com/pioneers/clinic/SummerClinicApplication.java
@@ -19,30 +19,30 @@ public class SummerClinicApplication {
 		SpringApplication.run(SummerClinicApplication.class, args);
 	}
 
-	@Bean
-	CommandLineRunner commandLineRunner() {
-		return arg -> {
-			playerService.addPlayer(new PlayerDto("Orooluwa", "Oluwatoyin", 11));
-			playerService.addPlayer(new PlayerDto("Akinmolayan", "Solomon", 14));
-			playerService.addPlayer(new PlayerDto("Bello", "David", 13));
-			playerService.addPlayer(new PlayerDto("Daniel", "Ozah", 8));
-			playerService.addPlayer(new PlayerDto("Yusuf", "Aliameen", 12));
-			playerService.addPlayer(new PlayerDto("Oluwadamilola", "Owolabi", 14));
-			playerService.addPlayer(new PlayerDto("Michael", "Egor", 13));
-			playerService.addPlayer(new PlayerDto("Tometi", "Oluwasemilore", 13));
-			playerService.addPlayer(new PlayerDto("Adeem", "Adenaya", 11));
-			playerService.addPlayer(new PlayerDto("Oreoluwa", "Adegbola", 12));
-			playerService.addPlayer(new PlayerDto("Chikanyima", "Benjamin", 13));
-			playerService.addPlayer(new PlayerDto("Deji", "Famuyibo", 13));
-			playerService.addPlayer(new PlayerDto("Oniha", "Mubarak", 14));
-			playerService.addPlayer(new PlayerDto("Ogben", "Praise", 13));
-			playerService.addPlayer(new PlayerDto("Ogben", "Shekinnah", 10));
-			playerService.addPlayer(new PlayerDto("Udochukwu", "Hope", 11));
-			playerService.addPlayer(new PlayerDto("Okeke", "William", 14));
-			playerService.addPlayer(new PlayerDto("Bakare", "Adeogo", 13));
-			playerService.addPlayer(new PlayerDto("Babalogbon Oluwadarasimi", "Emmanuel", 11));
-		};
-	}
+//	@Bean
+//	CommandLineRunner commandLineRunner() {
+//		return arg -> {
+//			playerService.addPlayer(new PlayerDto("Orooluwa", "Oluwatoyin", 11));
+//			playerService.addPlayer(new PlayerDto("Akinmolayan", "Solomon", 14));
+//			playerService.addPlayer(new PlayerDto("Bello", "David", 13));
+//			playerService.addPlayer(new PlayerDto("Daniel", "Ozah", 8));
+//			playerService.addPlayer(new PlayerDto("Yusuf", "Aliameen", 12));
+//			playerService.addPlayer(new PlayerDto("Oluwadamilola", "Owolabi", 14));
+//			playerService.addPlayer(new PlayerDto("Michael", "Egor", 13));
+//			playerService.addPlayer(new PlayerDto("Tometi", "Oluwasemilore", 13));
+//			playerService.addPlayer(new PlayerDto("Adeem", "Adenaya", 11));
+//			playerService.addPlayer(new PlayerDto("Oreoluwa", "Adegbola", 12));
+//			playerService.addPlayer(new PlayerDto("Chikanyima", "Benjamin", 13));
+//			playerService.addPlayer(new PlayerDto("Deji", "Famuyibo", 13));
+//			playerService.addPlayer(new PlayerDto("Oniha", "Mubarak", 14));
+//			playerService.addPlayer(new PlayerDto("Ogben", "Praise", 13));
+//			playerService.addPlayer(new PlayerDto("Ogben", "Shekinnah", 10));
+//			playerService.addPlayer(new PlayerDto("Udochukwu", "Hope", 11));
+//			playerService.addPlayer(new PlayerDto("Okeke", "William", 14));
+//			playerService.addPlayer(new PlayerDto("Bakare", "Adeogo", 13));
+//			playerService.addPlayer(new PlayerDto("Babalogbon Oluwadarasimi", "Emmanuel", 11));
+//		};
+//	}
 
 }
 
